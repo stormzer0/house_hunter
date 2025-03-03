c3506e608fb08adc544f9186c870a5185c171baa
JJ Alicea <144721874+stormzer0@users.noreply.github.com>
2025-03-03 00:45:22 -0500

    Update rentcast_to_firebase.py

--- rentcast_to_firebase.py
+++ rentcast_to_firebase.py
@@ -4,7 +4,7 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 # Load Firebase credentials (Replace with your Firebase key filename)
-cred = credentials.Certificate("your-firebase-key.json")  
+cred = credentials.Certificate("firebase-key.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
